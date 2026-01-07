1e69ed0636f67b48b547ab6163b8aad5e4d0e624
anhtt2211 <trananh.2211.work@gmail.com>
2026-01-07 14:10:23 +0700

    feat: add DialogOverlay component and animate DialogContent with open/closed states

--- Formula/audio-text.rb
+++ Formula/audio-text.rb
@@ -1,9 +1,9 @@
-class VideoToText < Formula
+class AudioText < Formula
   include Language::Python::Virtualenv
 
   desc "CLI tool to transcribe YouTube videos to text for IELTS practice"
-  homepage "https://github.com/anhtt2211/video-to-text"
-  url "https://files.pythonhosted.org/packages/source/v/video-to-text/video_to_text-0.1.0.tar.gz"
+  homepage "https://github.com/anhtt2211/audio-text"
+  url "https://files.pythonhosted.org/packages/source/a/audio-text/audio_text-0.1.0.tar.gz"
   sha256 "PLACEHOLDER_SHA256"
   license "MIT"
 
@@ -16,14 +16,14 @@ class VideoToText < Formula
   end
 
   # Add other resources here after publishing to PyPI
-  # Use: pip download video-to-text --no-binary :all: -d /tmp/packages
-  # Then generate resources with: poet video-to-text
+  # Use: pip download audio-text --no-binary :all: -d /tmp/packages
+  # Then generate resources with: poet audio-text
 
   def install
     virtualenv_install_with_resources
   end
 
   test do
-    assert_match "version", shell_output("#{bin}/video-to-text --version")
+    assert_match "version", shell_output("#{bin}/audio-text --version")
   end
 end
